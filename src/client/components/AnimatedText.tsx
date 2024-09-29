b8df6c510c4044a4a03e5f8af1b2ef7ede7113a5
Landry Geiger <landry.geiger@gmail.com>
2024-09-29 02:54:39 -0500

    Add animated text

---
+++ src/client/components/AnimatedText.tsx
@@ -0,0 +1,27 @@
+import { Typography, TypographySystem } from '@mui/joy';
+import { FC, useEffect, useRef, useState } from 'react';
+
+type Props = {
+  keyframes: string[];
+  periodMs: number;
+  typographyLevel: keyof TypographySystem;
+};
+
+const AnimatedText: FC<Props> = ({ keyframes, periodMs, typographyLevel }) => {
+  const [index, setIndex] = useState(0);
+  const keepGoingRef = useRef(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex(i => (i + 1) % keyframes.length);
+    }, periodMs);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <Typography level={typographyLevel}>{keyframes[index]}</Typography>;
+};
+
+export default AnimatedText;
